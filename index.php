8a184da9cb1d4a2f9d7c4217b354dc29163861f6
ahakala <andyhakala@gmail.com>
2013-12-06 13:44:03 -0600

    bootstrap added

--- index.php
+++ index.php
@@ -6,10 +6,21 @@
 function setFocus() {document.searchform.criteria.focus();}
 </script>
 
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link rel="stylesheet" type="text/css" href="default.css">
-<title>Active Directory Phonebook - Provided by techmonkeys.co.uk</title></head>
+<title>Active Directory Phonebook - Provided by techmonkeys.co.uk</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+</head>
 
 <body onLoad="setFocus()" topmargin="0" leftmargin="0" rightmargin="0" bottommargin="0"> 
 
@@ -292,6 +303,10 @@ echo "</table>";
 		</td>
 	</tr>
 </table>
+ <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 
 </body>
 
